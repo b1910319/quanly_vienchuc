87444e657b84a498111553796ead6d46997f8b42
b1910319 <ldtrinh2001@gmail.com>
2023-04-05 16:06:49 +0700

    sửa gia diện thongtin_vienchuc_edit

--- resources/views/vienchuc/thongtin_vienchuc_edit.blade.php
+++ resources/views/vienchuc/thongtin_vienchuc_edit.blade.php
@@ -1,7 +1,7 @@
 @extends('layout')
 @section('content')
   <div class="card-box">
-    <div class="alert alert-success row" role="alert" style="background-color: #3F979B; text-align: center;">
+    <div class="alert alert-success row color_alert" role="alert">
       <div class="col-1">
         @if ($phanquyen_qlk)
           <a href="{{ URL::to('/thongtin_vienchuc_khoa') }}" class="col-1">
@@ -24,17 +24,6 @@
     <form action="{{ URL::to('/update_thongtin_vienchuc/'.$edit->ma_vc) }}" method="POST"
       autocomplete="off" enctype="multipart/form-data">
       {{ csrf_field() }}
-      <?php
-        $message=session()->get('message');
-        if($message){
-          ?>
-            <p style="color: #379237" class="font-weight-bold text-center">
-              <?php echo $message ?>
-            </p>
-          <?php
-          session()->put('message',null);
-        }
-      ?>
       <div class="row">
         <div class="col-6">
           <table class="table">
@@ -561,8 +550,8 @@
         <div class="row mb-2">
           <div class="col-5"></div>
           <div class="col-2">
-            <button type="submit" class="btn btn-warning fw-bold" style="background-color: #FC7300; width: 100%">
-              <i class="fa-solid fa-pen-to-square"></i>
+            <button type="submit" class="btn btn-warning button_cam" style=" width: 100%">
+              <i class="fa-solid fa-pen-to-square text-light"></i>
               &ensp; Cập nhật
             </button>
           </div>
