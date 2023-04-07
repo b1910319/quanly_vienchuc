b3a4ede1288b7b5072be2c371e005908606bec30
b1910319 <ldtrinh2001@gmail.com>
2023-04-07 14:01:50 +0700

    xuất file excel thongke_qlcttc_thoihoc_loc_4_excel

---
+++ app/Exports/ThongKeQLCTTC_ThoiHoc_Loc_4Export.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Exports;
+
+use App\Models\VienChuc;
+use Maatwebsite\Excel\Concerns\FromQuery;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+
+class ThongKeQLCTTC_ThoiHoc_Loc_4Export implements FromQuery, WithHeadings, ShouldAutoSize
+{
+  /**
+   * @return \Illuminate\Support\Collection
+   */
+  public function __construct(int $ma_k, int $ma_l)
+  {
+    $this->ma_k = $ma_k;
+    $this->ma_l = $ma_l;
+    return $this;
+  }
+  public function headings(): array {
+    return [
+      'Mã số viên chức',
+      'Họ tên viên chức',
+      'Email',
+      'Số điện thoại',
+      'Ngày sinh',
+      'Khoa',
+      'Tên lớp', 
+      'Ngày bắt đầu học',
+      'Ngày kết thúc',
+      'Tên cơ sở đào tạo',
+      'Ngành học', 
+      'Trình độ đào tạo', 
+      'Email cơ sở đào tạo', 
+      'Số điện thoại cơ sở', 
+      'Ngày thôi học',
+      'Lý do thôi học'
+    ];
+  }
+  public function query()
+  {
+    return VienChuc::join('thoihoc', 'thoihoc.ma_vc', '=', 'vienchuc.ma_vc')
+    ->join('lop', 'lop.ma_l', '=', 'thoihoc.ma_l')
+    ->join('khoa', 'khoa.ma_k', '=', 'vienchuc.ma_k')
+    ->where('status_th', '<>', '2')
+    ->where('khoa.ma_k', $this->ma_k )
+    ->where('lop.ma_l', $this->ma_l )
+    ->where('status_th', '<>', '2')
+    ->where('status_vc', '<>', '2')
+    ->select('vienchuc.ma_vc', 'vienchuc.hoten_vc', 'vienchuc.user_vc', 'vienchuc.sdt_vc', 'vienchuc.ngaysinh_vc', 'khoa.ten_k', 'lop.ten_l', 'lop.ngaybatdau_l', 'lop.ngayketthuc_l', 'lop.tencosodaotao_l', 'lop.nganhhoc_l', 'lop.trinhdodaotao_l', 'lop.emailcoso_l', 'lop.sdtcoso_l', 'thoihoc.ngay_th', 'thoihoc.lydo_th' );
+  }
+}
--- app/Http/Controllers/ThongKeController.php
+++ app/Http/Controllers/ThongKeController.php
@@ -6598,6 +6598,21 @@ class ThongKeController extends Controller
       return Redirect::to('/home');
     }
   }
+  public function thongke_qlcttc_thoihoc_loc_4_excel($ma_k, $ma_l){
+    $this->check_login();
+    $ma_vc = session()->get('ma_vc');
+    $phanquyen_admin = PhanQuyen::where('ma_vc', $ma_vc)
+      ->where('ma_q', '=', '5')
+      ->first();
+    $phanquyen_qlcttc = PhanQuyen::where('ma_vc', $ma_vc)
+      ->where('ma_q', '=', '6')
+      ->first();
+    if($phanquyen_admin || $phanquyen_qlcttc){
+      return Excel::download(new ThongKeQLCTTC_ThoiHoc_Loc_4Export($ma_k, $ma_l), 'Vien-chuc-thoi-hoc.xlsx');
+    }else{
+      return Redirect::to('/home');
+    }
+  }
 
   public function thongke_qlcttc_thoihoc_loc_5_pdf($ma_k){
     $this->check_login();
