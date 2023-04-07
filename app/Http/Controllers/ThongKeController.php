acd0b1f153fce53c9e333572d594de08cfb0833a
b1910319 <ldtrinh2001@gmail.com>
2023-04-07 13:29:47 +0700

    xuất file excel thongke_qlcttc_chuyen_loc_2_excel

--- app/Http/Controllers/ThongKeController.php
+++ app/Http/Controllers/ThongKeController.php
@@ -6077,6 +6077,21 @@ class ThongKeController extends Controller
       return Redirect::to('/home');
     }
   }
+  public function thongke_qlcttc_chuyen_loc_2_excel($ma_k){
+    $this->check_login();
+    $ma_vc = session()->get('ma_vc');
+    $phanquyen_admin = PhanQuyen::where('ma_vc', $ma_vc)
+      ->where('ma_q', '=', '5')
+      ->first();
+    $phanquyen_qlcttc = PhanQuyen::where('ma_vc', $ma_vc)
+      ->where('ma_q', '=', '6')
+      ->first();
+    if($phanquyen_admin || $phanquyen_qlcttc){
+      return Excel::download(new ThongKeQLCTTC_Chuyen_Loc_2Export($ma_k), 'Vien-chuc-dung-hoc.xlsx');
+    }else{
+      return Redirect::to('/home');
+    }
+  }
 
   public function thongke_qlcttc_chuyen_loc_3_pdf($ma_l){
     $this->check_login();
--- routes/web.php
+++ routes/web.php
@@ -559,8 +559,10 @@ Route::get('/thongke_qlcttc_chuyen_loc_all_pdf/{ma_k}/{ma_l}',[ThongKeController
 Route::get('/thongke_qlcttc_chuyen_loc_all_excel/{ma_k}/{ma_l}',[ThongKeController::class, 'thongke_qlcttc_chuyen_loc_all_excel']);
 
 Route::get('/thongke_qlcttc_chuyen_loc_2_pdf/{ma_k}',[ThongKeController::class, 'thongke_qlcttc_chuyen_loc_2_pdf']);
+Route::get('/thongke_qlcttc_chuyen_loc_2_excel/{ma_k}',[ThongKeController::class, 'thongke_qlcttc_chuyen_loc_2_excel']);
 
 Route::get('/thongke_qlcttc_chuyen_loc_3_pdf/{ma_l}',[ThongKeController::class, 'thongke_qlcttc_chuyen_loc_3_pdf']);
+Route::get('/thongke_qlcttc_chuyen_loc_3_excel/{ma_l}',[ThongKeController::class, 'thongke_qlcttc_chuyen_loc_3_excel']);
 
 
 Route::post('/thongke_qlcttc_thoihoc_loc',[ThongKeController::class, 'thongke_qlcttc_thoihoc_loc']);
